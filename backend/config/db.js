3ae02387376b9466acafbbe3f675e098500d53a6
ShivanshuPandey <shivanshup05@gmail.com>
2026-05-06 13:01:55 +0530

    fixed railway db variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,12 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: "mysql",
   }
 );
