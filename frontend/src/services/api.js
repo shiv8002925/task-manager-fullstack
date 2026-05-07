436fc236a58c6ff38a14256f29231132bb067423
ShivanshuPandey <shivanshup05@gmail.com>
2026-05-07 13:27:54 +0530

    connected frontend to railway backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-manager-fullstack-production-9466.up.railway.app/api",
 });
 
 API.interceptors.request.use((req) => {
